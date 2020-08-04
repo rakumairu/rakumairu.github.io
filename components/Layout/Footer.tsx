97c0a9b44ec4e189cdf5dbc043c1bb54978f7ead
rakumairu <demaspira@outlook.com>
2020-08-04 23:45:55 +0700

    add timeline and footer

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,20 +1,27 @@
 const Footer = () => {
     return (
         // min-height temporary
-        <div className="w-full bg-main clip-top pt-20">
+        <div className="w-full bg-main clip-top pt-20 pb-4">
             <div className="container">
-                <h3 className="font-bold text-5xl mb-8 text-white">Contact Me</h3>
-                <div className="flex flex-col text-white items-start mb-6">
-                    <p className="text-lg leading-tight font-semibold">Demaspira Aulia</p>
-                    <a href="https://goo.gl/maps/SwoEksDpGUpk9d2q7" className="underline-link text-lg leading-tight font-semibold">Jakarta, Indonesia</a>
-                    <a href="mailto:rakumairu.dev@gmail.com" className="text-3xl leading-snug font-bold underline-link">rakumairu.dev@gmail.com</a>
-                </div>
-                <div className="flex items-center" style={{ filter: 'brightness(0) saturate(100%) invert(100%) sepia(3%) saturate(12%) hue-rotate(103deg) brightness(105%) contrast(105%)' }}>
-                    <p className="mr-2">Powered By:</p>
-                    <img src="/static/icons/nextjs-logo.svg" alt="nextjs-logo" className="h-8 mr-2" />
-                    <img src="/static/icons/tailwindcss-logo.svg" alt="tailwindcss-logo" className="h-8" />
-                    <div className="flex ml-auto">
-                        logo
+                <div className="mx-32">
+                    <h3 className="font-bold text-5xl mb-8 text-white">Contact Me</h3>
+                    <div className="flex flex-col text-white items-start mb-6">
+                        <p className="text-lg leading-tight font-semibold">Demaspira Aulia</p>
+                        <a href="https://goo.gl/maps/SwoEksDpGUpk9d2q7" className="underline-link text-lg leading-tight font-semibold">Jakarta, Indonesia</a>
+                        <a href="mailto:rakumairu.dev@gmail.com" className="text-3xl leading-snug font-bold underline-link">rakumairu.dev@gmail.com</a>
+                    </div>
+                    <div className="flex items-center" style={{ filter: 'brightness(0) saturate(100%) invert(100%) sepia(3%) saturate(12%) hue-rotate(103deg) brightness(105%) contrast(105%)' }}>
+                        <p className="mr-2">Powered By:</p>
+                        <img src="/static/icons/nextjs-logo.svg" alt="nextjs-logo" className="h-8 mr-2" />
+                        <img src="/static/icons/tailwindcss-logo.svg" alt="tailwindcss-logo" className="h-8" />
+                        <div className="flex ml-auto items-center">
+                            <a href="https://github.com/rakumairu">
+                                <img src="/static/icons/github.svg" alt="linkedin" className="w-8 mr-4 transition-transform duration-150 ease-linear transform hover:-translate-y-1" />
+                            </a>
+                            <a href="https://www.linkedin.com/in/demaspira/">
+                                <img src="/static/icons/linkedin.svg" alt="linkedin" className="w-8 transition-transform duration-150 ease-linear transform hover:-translate-y-1" />
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
 import Main from "../components/Layout/Main";
+import { IMeta } from '../utils/Interfaces';
 
-export default function Home() {
+const meta: IMeta = {
+    title: "Hi! Welcome to my page | Rakumairu DEV",
+    description: "Hello there, I'm a Frontend Developer, you can see my work and my contact here, I also have some article that might interest you",
+    url: process.env.NEXT_PUBLIC_URL
+}
+
+const Home = () => {
     return (
-        <Main>
+        <Main
+            meta={meta}
+        >
             <div className="relative h-screen pb-32 bg-gray-ea">
                 <div className="h-full bg-cover bg-no-repeat" style={{ backgroundImage: 'url(/static/images/splash.jpg)', filter: 'brightness(.5)' }} />
                 <div className="absolute inset-0 flex flex-col items-center justify-center pb-40">
@@ -25,17 +34,17 @@ export default function Home() {
                     <div className="card bg-main-dark text-white rounded shadow-5-layer flex flex-col items-start p-4 mx-8 transition duration-200 ease-linear hover:bg-accent hover:text-black" style={{ width: '25%' }}>
                         <h5 className="text-2xl font-semibold pb-2 mb-4 border-b-2 border-white">Job Offer?</h5>
                         <p className="text-white opacity-75 mb-8">I'm currently not open for job but let's keep in touch!</p>
-                        <button className="mt-auto bg-white rounded-lg px-4 py-2 text-black text-lg font-semibold shadow-4-layer">Contact Me!</button>
+                        <button className="mt-auto bg-white rounded-lg px-4 py-2 text-black text-lg font-semibold shadow-4-layer outline-none focus:outline-none">Contact Me!</button>
                     </div>
                     <div className="card bg-main-dark text-white rounded shadow-5-layer flex flex-col items-start p-4 mx-8 transition duration-200 ease-linear hover:bg-accent hover:text-black" style={{ width: '25%' }}>
                         <h5 className="text-2xl font-semibold pb-2 mb-4 border-b-2 border-white">Dedicated Project?</h5>
                         <p className="text-white opacity-75 mb-8">If you have a project that you want to realize, let's discuss it together!</p>
-                        <button className="mt-auto bg-white rounded-lg px-4 py-2 text-black text-lg font-semibold shadow-4-layer">Hit Me Up!</button>
+                        <button className="mt-auto bg-white rounded-lg px-4 py-2 text-black text-lg font-semibold shadow-4-layer outline-none focus:outline-none">Hit Me Up!</button>
                     </div>
                     <div className="card bg-main-dark text-white rounded shadow-5-layer flex flex-col items-start p-4 mx-8 transition duration-200 ease-linear hover:bg-accent hover:text-black" style={{ width: '25%' }}>
                         <h5 className="text-2xl font-semibold pb-2 mb-4 border-b-2 border-white">Just Wanna be Friends?</h5>
                         <p className="text-white opacity-75 mb-8">Let's be friends and talk about all the things that we like!</p>
-                        <button className="mt-auto bg-white rounded-lg px-4 py-2 text-black text-lg font-semibold shadow-4-layer">DM Me!</button>
+                        <button className="mt-auto bg-white rounded-lg px-4 py-2 text-black text-lg font-semibold shadow-4-layer outline-none focus:outline-none">DM Me!</button>
                     </div>
                 </div>
             </div>
@@ -113,14 +122,92 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <div className="pb-48 bg-white">
+            <div className="pb-32 bg-white">
                 <div className="container">
                     <div className="mx-32">
-                        <h3 className="font-bold section-header text-black">Experience .</h3>
+                        <h3 className="font-bold section-header text-black mb-10">Experience .</h3>
+                        <div className="w-full flex flex-col">
+                            <div className="flex timeline">
+                                <div className="w-1/2 flex justify-end">
+                                    <div className="w-auto flex items-center justify-center px-8 section-header font-bold text-black text-opacity-25 transition duration-200 ease-in-out hover:text-main hover:text-opacity-100 cursor-default">
+                                        2018
+                                    </div>
+                                    <div className="w-8 h-full bg-main flex flex-col items-center justify-center text-lg text-white font-bold leading-none px-2 py-8 text-center">
+                                        ~
+                                    </div>
+                                </div>
+                                <div className="w-1/2 flex justify-start">
+                                    <div className="h-full bg-white flex flex-col text-lg px-2 py-8 justify-center">
+                                        <h5 className="text-xl font-semibold text-main">Process Mining Research</h5>
+                                        <p className="mb-4 text-black-48">Contribute in my lecturer's research as Fullstack Developer about process mining.</p>
+                                        <h5 className="text-xl font-semibold text-main">Aid Other Final Project</h5>
+                                        <p className="mb-4 text-black-48">Develop an app for warehouse administration for a Civil Engineering student.</p>
+                                        <h5 className="text-xl font-semibold text-main">Started My Final Project</h5>
+                                        <p className="text-black-48">Started my Final Project for graduation about process mining which use Angular for the frontend and using Flask for the backend and calculation.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex">
+                                <div className="w-1/2 flex justify-start order-2">
+                                    <div className="w-8 h-full bg-accent flex flex-col items-center justify-center text-lg text-white font-bold leading-none px-2 py-8 text-center">
+                                        ~
+                                    </div>
+                                    <div className="w-auto flex items-center justify-center px-8 section-header font-bold text-black text-opacity-25 transition duration-200 ease-in-out hover:text-accent hover:text-opacity-100 cursor-default">
+                                        2019
+                                    </div>
+                                </div>
+                                <div className="w-1/2 flex justify-end order-1">
+                                    <div className="h-full bg-white flex flex-col text-lg px-2 py-8 text-right justify-center">
+                                        <h5 className="text-xl font-semibold text-accent">Finish My Final Project</h5>
+                                        <p className="mb-4 text-black-48">Finishes my final project for graduation and publish the results as a journal article in <a className="text-accent hover:text-accent-dark" href="http://journals.ums.ac.id/index.php/khif">KHIF</a>, and here is the link to <a className="text-accent hover:text-accent-dark" href="https://www.researchgate.net/publication/339382239_The_Design_of_Exploratory_Application_and_Preprocessing_of_Event_Log_Data_in_LMS_Moodle-Based_Online_Learning_Activities_for_Process_Mining">My Article</a>.</p>
+                                        <h5 className="text-xl font-semibold text-accent">Fraud Detection Research</h5>
+                                        <p className="mb-4 text-black-48">Contribute in my lecturer's research as Frontend Developer about fraud detection using Isolation Forest Algorithm.</p>
+                                        <h5 className="text-xl font-semibold text-accent mb-4">Graduated College!!!</h5>
+                                        <h5 className="text-xl font-semibold text-accent">First Full-time Work</h5>
+                                        <p className="text-black-48">I join Emporia Digital as Frontend Developer, we're using Laravel and Lumen here.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex">
+                                <div className="w-1/2 flex justify-end">
+                                    <div className="w-auto flex items-center justify-center px-8 section-header font-bold text-black text-opacity-25 transition duration-200 ease-in-out hover:text-main hover:text-opacity-100 cursor-default">
+                                        2020
+                                    </div>
+                                    <div className="w-8 h-full bg-main flex flex-col items-center justify-center text-lg text-white font-bold leading-none px-2 py-8 text-center">
+                                        ~
+                                    </div>
+                                </div>
+                                <div className="w-1/2 flex justify-start">
+                                    <div className="h-full bg-white flex flex-col text-lg px-2 py-8 justify-center">
+                                        <h5 className="text-xl font-semibold text-main">Mutated to Child Company</h5>
+                                        <p className="mb-4 text-black-48">Got mutated to other child company as fullstack developer and network engineering.</p>
+                                        <h5 className="text-xl font-semibold text-main">Join Jendela360</h5>
+                                        <p className="mb-4 text-black-48">Join <a className="text-main hover:text-main-dark" href="https://jendela360.com/">Jendela360</a> as Frontend Developer, we're using React and NextJs here.</p>
+                                        <h5 className="text-xl font-semibold text-main">Create My First Portfolio Page!!!</h5>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex">
+                                <div className="w-1/2 flex justify-start order-2">
+                                    <div className="w-8 h-full bg-accent flex flex-col items-center justify-center text-lg text-white font-bold leading-none px-2 py-8 text-center">
+                                        .<br/>.<br/>.
+                                    </div>
+                                    <div className="w-auto flex items-center justify-center px-8 section-header font-bold text-black text-opacity-25 transition duration-200 ease-in-out hover:text-accent hover:text-opacity-100 cursor-default">
+                                        2021
+                                    </div>
+                                </div>
+                                <div className="w-1/2 flex justify-end order-1">
+                                    <div className="h-full bg-white flex flex-col text-lg px-2 py-8 text-right justify-center">
+                                        <h5 className="text-xl font-semibold text-accent">Let's see where I'll go next!</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </Main>
-        )
-    }
+    )
+}
     
+export default Home
