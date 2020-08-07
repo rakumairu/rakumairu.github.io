1d86faa2492585954484c93f62b2e79ea78efb99
rakumairu <demaspira@outlook.com>
2020-08-07 19:28:51 +0700

    fix font bugs

--- components/Layout/Main.tsx
+++ components/Layout/Main.tsx
@@ -49,7 +49,7 @@ const Main = ({ children, meta }: IProps) => {
 
                 <link rel="preconnect" href="https://fonts.gstatic.com/" />
                 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet" />
-                <link href="\https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,400&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,400&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet" />
 
                 {/* <link rel="preconnect" href="https://www.google-analytics.com" /> */}
